1bde79b1b7a0474666e83b5b35f7cf7156c3486d
bud aminof <budaminof@gmail.com>
2016-02-16 13:02:27 -0700

    finished box query playground

--- boxes.js
+++ boxes.js
@@ -1,1 +1,16 @@
+
 console.log("hello world");
+
+$(document).ready(function () {
+  alert("ready for DOM manipulation");
+
+  $('#secretBox').css({'background-color': 'white'});
+  $('#secretBox').prepend('<h1>secret box!</h1>');
+  $('#row1').children().addClass('boxType3');
+  $('#row4 div:last-child').css('display', 'none');
+  $('.boxType1').css({'background-color':'white'});
+  $('#row2 div:nth-child(1)').removeClass();
+  $('#row2 div:nth-child(2)').removeClass();
+  $('.box:not(#secretBox)').css({'width': '2px'});
+  
+});
