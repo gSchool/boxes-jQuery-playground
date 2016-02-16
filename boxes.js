7cdf6b7c4465784130fe92442a66203a85327d0d
Anthony Simpson <ansi6622@colorado.edu>
2016-02-16 12:36:32 -0700

    about halfway thru first repo

--- boxes.js
+++ boxes.js
@@ -1,4 +1,16 @@
 $(function() {
-    console.log( "ready!" );
+  console.log( "ready!" );
+  $('#secretBox').on('mouseover', function(){
+    $(this).css({'background-color': 'white', 'border-bottom': '1px solid black'})
+  }).append('<h1>Secret box!</h1>');
+  $('#row1 > div').on('mouseover', function(){
+    $(this).addClass('boxType3')
+  });
+  $('#row1 > div:last-child').on('mouseover', function(){
+    $(this).css({'display': 'none'})
+  });
+  $('.boxType1').on('mouseover', function(){
+    $(this).css({'background-color': 'white', 'border-bottom': '1px solid black'})
+  });
 });
 // console.log("hello world");
