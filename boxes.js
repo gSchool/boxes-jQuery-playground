9811f86be11b369eaf9301187c332fff303d1d3f
Anthony Simpson <ansi6622@colorado.edu>
2016-02-16 14:59:54 -0700

    almost done boxing but i need a bit more

--- boxes.js
+++ boxes.js
@@ -9,8 +9,17 @@ $(function() {
   $('#row1 > div:last-child').on('mouseover', function(){
     $(this).css({'display': 'none'})
   });
-  $('.boxType1').on('mouseover', function(){
-    $(this).css({'background-color': 'white', 'border-bottom': '1px solid black'})
+  $('.boxType1').css({'background-color': 'white', 'border-bottom': '1px solid black'}).append("<a href='http://www.galvanize.com/'>Click Me</a>")
+  $('#row2 > div:first-child, #row2 > div:first-child+div').css({'all': 'initial'});
+  $(" #container > div:not(.row), #container > div:not(#secretBox)").css({'width': '2px'});
+  $('#container').on('click', function(e) {
+        console.log(e.pageX + ' , ' + e.pageY);
+    });
+  $('.boxType1').on('click', function(){
+    alert("You can never leave the page");
   });
+
+
+
 });
 // console.log("hello world");
