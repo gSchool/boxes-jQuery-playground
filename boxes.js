58313809fe82fe7b58039cb812c69365a4a1ee42
Brad Butterfield <butters5789@gmail.com>
2016-02-16 12:46:37 -0700

    Exercise 2 - Selectors, CSS, Attributes

--- boxes.js
+++ boxes.js
@@ -1,9 +1,10 @@
-//console.log("hello world");
-
 $( document ).ready(function() {
-    alert("I am ready for DOM manipulation");
-});
+    alert( 'I am ready for DOM manipulation' );
+    $( '#secretBox' ).css({ 'background-color': 'white' }).append( '<h1>secret box!</h1>' );
+    $( '#row1 > div' ).addClass( 'boxType3' ).removeClass( 'boxType1' );
+    $( '#row4 > div:last-child' ).css({ 'display': 'none' });
+    $( '.boxType1' ).css({ 'background-color': 'white' });
+    $( '#row2 > div:nth-child(-n+2)' ).css({ 'all': 'unset' });
+    $( '#container > .row > .box:not(#secretBox)' ).css({ 'width': '2px' });
 
-$(function() {
-    alert("Callback style 2: I am ready for DOM manipulation");
 });
