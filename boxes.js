3bad6f2273da7bfdfbc7b7fbcdf90b28637e4121
Anthony Simpson <ansi6622@colorado.edu>
2016-02-16 15:18:34 -0700

    on puppies

--- boxes.js
+++ boxes.js
@@ -1,7 +1,7 @@
 $(function() {
   console.log( "ready!" );
   $('#secretBox').on('mouseover', function(){
-    $(this).css({'background-color': 'white', 'border-bottom': '1px solid black'})
+    $(this).css({'background-color': 'white', 'border-bottom': '4px solid orange'})
   }).append('<h1>Secret box!</h1>');
   $('#row1 > div').on('mouseover', function(){
     $(this).addClass('boxType3')
@@ -11,12 +11,18 @@ $(function() {
   });
   $('.boxType1').css({'background-color': 'white', 'border-bottom': '1px solid black'}).append("<a href='http://www.galvanize.com/'>Click Me</a>")
   $('#row2 > div:first-child, #row2 > div:first-child+div').css({'all': 'initial'});
-  $(" #container > div:not(.row), #container > div:not(#secretBox)").css({'width': '2px'});
+  // $(" #container > div:not(.row), #container > div:not(#secretBox)").css({'width': '2px'});
   $('#container').on('click', function(e) {
         console.log(e.pageX + ' , ' + e.pageY);
     });
-  $('.boxType1').on('click', function(){
+  $('.boxType1').on('click', function(e){
     alert("You can never leave the page");
+    e.preventDefault();
+  });
+  $('.box').on('click', function(){
+    $(this).addClass('puppy');
+    $('.puppy').css({("background-image": "url('https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRJTJk1vRYIzRaAzigs2A-338F-fi308r2ySl4DqXyzmU56lBSiFPJ9CSw')"});
+
   });
 
 
