97067b04b4fcdc071cf08718410cbd6cd3d6a0b9
Daniel Stewart <daniel.robbins.stewart@gmail.com>
2016-02-16 13:51:02 -0700

    Add code for all exercises up to 3.4

--- boxes.js
+++ boxes.js
@@ -1,1 +1,42 @@
 console.log("hello world");
+
+$(function() {
+  alert("Ready for DOM manipulation");
+
+  // $('#secretBox').css({'background-color': 'white'});
+  // $('#secretBox').append('<h1>Secret Box!</h1>');
+  //
+  // $('#row1').children().addClass('boxType3');
+  //
+  // $('#row4').children().last().hide();
+  //
+  // $('.boxType1').css({'background-color': 'white'});
+  //
+  // $('#row2 div:nth-child(1)').removeClass();
+  // $('#row2 div:nth-child(2)').removeClass();
+  //
+  // $('.box:not(#secretBox)').css({'width': '2px'});
+
+  $('#container').on('click', function(e){
+    console.log(e.pageX + ", " + e.pageY)
+  });
+
+  $('.boxType1').wrap('<a href="http://www.galvanize.com/"></a>');
+  $('a').on('click', function(event){
+    event.preventDefault();
+    alert("You can never leave! MUAHAHAHAHA!!!!")
+  });
+
+
+  $('.box').append('<img src="http://loremflickr.com/150/150/dog" alt="dog">');
+  $('.box').children().hide();
+
+  $('.box').on('click', function(){
+    $(this).children().toggle();
+  });
+
+  $('#container').on('click', function(){
+    $('#container').css({'background-color': 'black'});
+  });
+
+});
