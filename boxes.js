9624f927cd697349bd186c897c23a09dd64e5664
Phil Skaggs <prskaggsjr@gmail.com>
2016-02-16 13:12:40 -0700

    finish exercise 2

--- boxes.js
+++ boxes.js
@@ -6,4 +6,19 @@ $(document).ready(function(){
 
 $(function() {
   console.log( "double ready! ");
+
+  $('#secretBox').css({'background-color': 'white'});
+
+  $('#secretBox').append( '<h1>secret box!</h1>' );
+
+  $( '#row1' ).find( 'div' ).addClass( 'boxType3' );
+
+  $( '#row4 div' ).last().hide();
+
+  $( '.boxType1:not(.boxType3)' ).css({'background-color': 'white'});
+
+  $( '#row2' ).children().slice( 0,2 ).removeClass();
+
+  $( '.box:not(#secretBox)').width('2px');
+
 });
