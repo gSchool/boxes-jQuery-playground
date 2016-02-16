e65ff06c101d57420f07e02ea678e50473587144
Cooper Heinrichs <cooper.heinrichs@gmail.com>
2016-02-16 16:37:54 -0700

    Added last event listener for exercise 4

--- boxes.js
+++ boxes.js
@@ -1,34 +1,38 @@
 $(function(){
-
+  //********* 1 **********//
   /*$('#container').on('click', function(e){
     console.log("x: " + e.pageX + ", y: " + e.pageY);
   });*/
 
-/*
-  $('.box').on('click', function(){
-    if($(this).hasClass("puppyImage")){
-      $(this).removeClass("puppyImage").css({"padding-top":"10px"});
-    } else {
-      $(this).addClass("puppyImage").css({"padding-top":"0px"});
-    }
-  });
-*/
-  $('.boxType1').wrap("<a href='http://www.galvanize.com/'></a>");
+
+  //********* 2 **********//
+  /*$('.boxType1').wrap("<a href='http://www.galvanize.com/'></a>");
 
   $('#container').on('click', function(event){
     console.log("no leave");
     alert("NU UH");
     event.preventDefault();
-  });
+  });*/
 
-/*
-
-  document.getElementById("container").addEventListener("click", function(){
-    if(){
-
+  //********* 3 **********//
+  /*$('.box').on('click', function(){
+    if($(this).hasClass("puppyImage")){
+      $(this).removeClass("puppyImage").css({"padding-top":"10px"});
+    } else {
+      $(this).addClass("puppyImage").css({"padding-top":"0px"});
     }
-    this.style.backgroundColor = "black";
-    console.log("CLICKED THE CONTAINER");
+  });*/
+
+  //********* 4 **********//
+  document.getElementById("container").addEventListener("click", function(event){
+
+    if( event.target.id === "container" ){
+      event.target.style.backgroundColor = "#00ff00";
+    } else {
+      event.target.style.backgroundColor = "white";
+      this.style.backgroundColor = "black";
+    }
+
   }, false);
-*/
+
 });
