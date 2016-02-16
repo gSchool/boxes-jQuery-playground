f611a9910896aeb79f26e5f5dd2c8d87d1aa3289
Brad Butterfield <butters5789@gmail.com>
2016-02-16 11:51:29 -0700

    Exercise 1 - DOM Ready

--- boxes.js
+++ boxes.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+//console.log("hello world");
+
+$( document ).ready(function() {
+    alert("I am ready for DOM manipulation");
+});
+
+$(function() {
+    alert("Callback style 2: I am ready for DOM manipulation");
+});
