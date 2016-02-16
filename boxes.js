d9d9e923612eaceff2b10ae6de8890aabe4962b9
Summer Wollin <summer.wollin@gmail.com>
2016-02-16 12:42:42 -0700

    Solution for exercise 1, exercise 2

--- boxes.js
+++ boxes.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+$(function() {
+    alert( "ready for DOM manipulation" );
+    $("#secretBox").css({backgroundColor: 'white'}).prepend('<h1>secret box!</h1>');
+    $("#row1").children().attr('class', 'box boxType3');
+    $('#row4').children().last().hide();
+    $('.boxType1').css({backgroundColor: 'white'});
+    $('#row2').children().slice(0,2).attr('class', 'blop');
+    $('#container div').not(".row").not("#secretBox").css({width: '2px'});
+});
