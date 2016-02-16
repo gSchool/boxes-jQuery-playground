def4ee29273a5eabd510a6f9c5403f5dbae4a4ab
Coleman Kelleghan <coleman@kelleghandesign.com>
2016-02-16 16:40:20 -0700

    Box playground complete

--- boxes.js
+++ boxes.js
@@ -1,1 +1,56 @@
-console.log("hello world");
+$(document).ready(function(){
+  console.log('ready');
+
+  // $('#secretBox').css('backgroundColor', 'white');
+  // $('#secretBox').append('<h1>Secret Box</h1>');
+  // $('#row1').children().addClass('boxType3');
+  //
+  // // $('#row4').children().last().css('display', 'none');
+  //
+  // $('.boxType1').css('backgroundColor', 'white');
+  //
+  // $('#row2 div:nth-child(2)').removeClass();
+  // $('#row2 div:nth-child(1)').removeClass();
+
+
+  // $(".box").not("#secretBox").css("width", "2px");
+
+
+
+  //
+  // $('#container').on('click', function(e){
+  //   console.log(e.pageX)
+  //   console.log(e.pageY)
+  // });
+  //
+  //
+  // $('.boxType1').wrap('<a href="http://www.galvanize.com/"/></a>');
+  //
+  // $('.boxType1').on('click', function(){
+  //   alert('you can never leave')
+  // });
+  //
+  //
+
+
+  //
+  // $('.box').append('<img src="http://loremflickr.com/100/100" />');
+  //
+  //
+  // $('div .box img').on('click',function(e){
+  //   $(e.target).remove();
+  // });
+
+
+  // 
+  // $('#container').on('click', function(e){
+  //   $('#container').css('backgroundColor', 'black')
+  //   $(e.target).css('backgroundColor', 'white');
+  // });
+  //
+  //
+
+
+
+
+}); // ready
