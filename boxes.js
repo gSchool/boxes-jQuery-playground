dd8df0b28237210a29c091e933910487d0554a86
Mickey <develop.agility@gmail.com>
2016-02-16 17:12:41 -0700

    a lil fine tuning for exercise 3

--- boxes.js
+++ boxes.js
@@ -1,34 +1,31 @@
 
 $(function(){
-  console.log("ready");
 
   $('#container').on('click', function(e){
 
-    if(!$(event.target).is($(this))){
+    if(!$(e.target).is($(this))){
 
-      $(event.target).css('background','white');
+      $(e.target).css('background','white');
       $(this).css('background','black');
 
     } else
 
       $(this).css('background','lime');
-
   });
 
 
-  $( ".boxType1").append("<a href='http://www.galvanize.com'>Galvanize</a>")
+  $(".boxType1").append("<a href='http://www.galvanize.com'>Galvanize</a>")
       .on('click', function(){
 
-        if(alert("Sorry you can't leave")){
-        }else
-          $('a').attr('href', 'http://localhost:63342/boxes-jQuery-playground/boxes.html');
+        alert("Sorry you can't leave");
+          event.preventDefault();
   });
 
 
   $('div .box').append("<img style='display: none' src='http://wallpaperhdbase.com/wp-content/gallery/ugly-puppy-pictures/Old+Ugly+Chi.jpg' height='140px'>")
       .on('click', function(){
 
-      $(this).children().toggle('fast');
+        $(this).children().toggle('fast');
   });
 
 });
