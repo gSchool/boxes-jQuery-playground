90d821dfe825132aa46a7e66cfef5d0e84e1c62a
James Freeman <freeman.james.h@gmail.com>
2016-02-16 19:33:54 -0700

    removed bottom right box

--- boxes.js
+++ boxes.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   alert("Hello, world!");
   $('#secretBox').css({'backgroundColor':'white'}).append("<h1>I'm a secret box!</h1>");
   $('#row1').children().removeClass().addClass('box boxType3');
-  $('#row4 > div:last').css({'backgroundColor':'black'});
+  $('#row4 > div:last').css({'display':'none'});
 });
 
 
