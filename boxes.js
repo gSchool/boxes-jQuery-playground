1d0ca75c130a3e799b7b764c5c93ffbce4a8e58d
James Freeman <freeman.james.h@gmail.com>
2016-02-16 19:17:33 -0700

    changed top row to boxType3, secret box discovered/labeled

--- boxes.js
+++ boxes.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
   alert("Hello, world!");
+  $('#secretBox').css({'backgroundColor':'white'}).append("<h1>I'm a secret box!</h1>");
+  $('#row1').children().removeClass().addClass('box boxType3');
 });
+
 
 
 
