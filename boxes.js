edc7a2ff5680352a75eabcd3ee0305fdfc2c33db
Roy <rechase84@gmail.com>
2016-06-06 15:18:39 -0600

    init commit

--- boxes.js
+++ boxes.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+'use strict';
+//variable declaration
+// var $secretBox;
+// $secretBox = ('#secretBox');
+
+
+//alert when loaded
+$(document).ready(alertReady);
+	console.log('document is ready');
+	function alertReady(){
+		alert('ready for DOM manipulation');
+}
+
+$('#secretBox').ready(function(){
+	console.log('entering secretBox');
+	$('#secretBox').css('background-color', 'white');
+	$('#secretBox').append('<h1>Secret Box!</p>');
+});
