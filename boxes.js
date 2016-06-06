5edeee42c35e4142750b67ec165f77a37c58c6cc
Roy <rechase84@gmail.com>
2016-06-06 15:49:24 -0600

    up to changing classes

--- boxes.js
+++ boxes.js
@@ -1,7 +1,5 @@
 'use strict';
-//variable declaration
-// var $secretBox;
-// $secretBox = ('#secretBox');
+
 
 
 //alert when loaded
@@ -11,8 +9,20 @@ $(document).ready(alertReady);
 		alert('ready for DOM manipulation');
 }
 
-$('#secretBox').ready(function(){
+//change css and add h1 to secretBox
+$(document).ready(function(){
 	console.log('entering secretBox');
 	$('#secretBox').css('background-color', 'white');
 	$('#secretBox').append('<h1>Secret Box!</p>');
+
+//Find all child divs of the first row.  Set the class for each div to boxType3.
+	console.log('finding all child divs of first row');
+	var row1 = $('#row1').children();
+	console.log(row1);
+	$(row1).toggleClass('boxType3');
 });
+
+// $('#row4 boxType1').on('mouseover', function(){
+// 	console.log('deleting last div of last column');
+// 	$('div').remove();
+// });
