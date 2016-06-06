96b06ad5d8c2f1fe5d539c2b7d6788cc15d5652e
Jeremy <jkaffrey@gmail.com>
2016-06-06 15:24:33 -0600

    Done upto Exercise 2

--- boxes.js
+++ boxes.js
@@ -1,1 +1,32 @@
-console.log("hello world");
+'use strict';
+
+$(function () {
+
+  alert('DOM ready!');
+
+  var $secret = $('#secretBox');
+  $secret.css('background-color', 'white');
+  $secret.append('<h1>secret box!</h1>');
+
+  $('#row1 div').each(function() {
+
+    $(this).toggleClass('boxType3');
+  });
+
+  $('#row4 div:last-child').css('display', 'none');
+
+  $('.boxType1').each(function() {
+
+    $(this).css('background-color', 'white');
+  });
+
+  $('#row2 div:nth-child(-n+2)').each(function() {
+
+    $(this).removeClass();
+  });
+
+  $('#container div:not(.row)').not('#secretBox').each(function() {
+
+    $(this).css('width', '2px');
+  });
+});
