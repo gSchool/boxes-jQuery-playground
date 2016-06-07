b8158e1b8217a7d1912dd3c39596014cacc4f836
vince vahala <vincevahala@gmail.com>
2016-06-07 14:36:33 -0600

    Exercise 1 and 2 complete

--- boxes.js
+++ boxes.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+$(document).ready(function() {
+    $('#secretBox').css({'background-color':'white'});
+    $('#secretBox').html('<h1>Secret Box!</h1>');
+    $('#row1 div').addClass("boxType3")
+    $('#row4 .box:nth-child(4)').css({'display':'none'});
+    $('.boxType1').css({'background-color':'white'})
+    $('#row2 div:nth-child(-n+2)').removeClass('box');
+    $('#container div').not('div .row').not('#secretBox').css({'width':'2px'});
+});
