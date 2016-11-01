4462ddfc38e6e130d56b7677ee0e27caca02f8ae
Dillon Corkran <dcorkran972@gmail.com>
2016-11-01 00:17:00 -0600

    finished pt 1 and 2 - commented out part 2 solutions

--- boxes.js
+++ boxes.js
@@ -1,1 +1,14 @@
+$(document).ready(function() {
+  // alert('ready for DOM manipulation');
+  // $('#secretBox').css('background-color','white');
+  // $('#secretBox').append('<h1>secret box!</h1>');
+  // $('#row1 > div').removeClass('boxType2 boxType1').addClass('boxType3');
+  // $('.boxType1').css('background','white');
+  // $('#row4 div:nth-child(4)').css('visibility','hidden');
+  // $('#row2 div:nth-child(1)').removeClass('box boxType1 boxType3 boxType2');
+  // $('#row2 div:nth-child(2)').removeClass('box boxType1 boxType3 boxType2');
+  // $('.box:not(#secretBox)').css('width','2px');
+  
+});
+
 console.log("hello world");
