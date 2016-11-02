35e6d6e358dce698ff2617c0f41c2d36e418c191
Murph Grainger <murph.grainger@gmail.com>
2016-11-02 16:57:58 -0600

    finished through Exercise 2

--- boxes.js
+++ boxes.js
@@ -1,1 +1,10 @@
 console.log("hello world");
+
+$('#secretBox').append('<h1>secret box!</h1>');
+$('#row1').children().addClass('boxType3');
+$('#row4 > div:last-child').hide();
+$('div.box.boxType1').css('backgound-color', 'white');
+$('#row2 > div:lt(2)').removeAttr('style');
+$('#row2 > div:lt(2)').removeAttr('class');
+$('.box').not(document.getElementById("secretBox"))
+    .css("width", "2px");
