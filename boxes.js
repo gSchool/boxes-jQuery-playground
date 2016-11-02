3f207ed310ed36185d5f873a5b16ee9fc3a1778a
Dillon Corkran <dcorkran972@gmail.com>
2016-11-01 19:46:35 -0600

    completed funbox exercises

--- boxes.js
+++ boxes.js
@@ -8,7 +8,37 @@ $(document).ready(function() {
   // $('#row2 div:nth-child(1)').removeClass('box boxType1 boxType3 boxType2');
   // $('#row2 div:nth-child(2)').removeClass('box boxType1 boxType3 boxType2');
   // $('.box:not(#secretBox)').css('width','2px');
-  
-});
 
-console.log("hello world");
+  // $('#container').on('click',function(e){
+  //   var xPos = e.clientX;
+  //   var yPos = e.clientY;
+  //   console.log('xposition = '+ xPos, 'yposition = '+yPos);
+  // });
+  // $('.boxType1').html('<a href="http://galvanize.com">Galvanize</a>');
+  // $('a').on('click',function(){
+  //   alert('no leaving!!!');
+  //   event.preventDefault();
+  // });
+
+
+  // $('.box').on('click',function(){
+  //   var clicked = $(this);
+  //   if (clicked.css('background-image') === 'none') {
+  //     clicked.css('background-image',"url('http://www.successpuppytraining.com/wp-content/uploads/2014/09/testimony-monty-50x50.jpg')");
+  //   } else {
+  //     clicked.css('background-image','');
+  //   }
+  // });
+
+  $('#container').on('click',function(e){
+    if (e.target === this) {
+      $(this).css('background-color','lime');
+    } else {
+      $(this).css('background-color','black');
+      $(e.target).css('background-color','white');
+    }
+  });
+
+  console.log("hello world");
+
+});
