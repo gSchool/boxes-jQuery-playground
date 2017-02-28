89c3a22c47b256e6429a45c8a8253adc60598550
GJullian Flemister-King <gjullianfk@gmail.com>
2017-02-28 16:47:34 -0700

    done

--- boxes.js
+++ boxes.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+$(document).ready(function(){
+  $('#secretBox').css("background-color","white");
+  $('#secretBox').html("<h1>Secret Box!</h1>");
+  $('#row1').children().attr('class', 'box boxType3');
+  console.log($('#row4 div')[3].remove());
+  $('.boxType1').css('background-color','white');
+  $('#container div:not(#secretBox), #container div:not(.row)').css('width','2px');
+
+});
