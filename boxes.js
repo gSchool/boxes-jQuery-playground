ffd2e8ef01d6d31959973c48f45609b2113b629b
Dashon-Hawkins <dashon.hawkins@gmail.com>
2017-02-28 17:23:53 -0700

    Dashon Hawkins commit

--- boxes.js
+++ boxes.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+
+$(document).ready(function() {
+  console.log("hello ready");
+
+  $('#secretBox').css("background-color", "white");
+  $('#secretBox').html("<h1>secrect box</h1>");
+  $('#row1').children().addClass("boxType3");
+  $('#row4 div:last-child').css("display", "none");
+  $('.boxType1').css("background", "white");
+  $('#row2').children().slice(0, 2).remove();
+  //$('#row2').eq().removeClass();
+  //$("div").not(".row #secretBox").css("width", "2px");
+
+})
